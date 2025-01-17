cf73f32996040c4be3d25ad9c7ff8f1511f74bf9
Jacob Koziej <jacobkoziej@gmail.com>
2025-01-16 23:32:46 -0500

    Add `python3Packages.torch`

--- flake.nix
+++ flake.nix
@@ -18,14 +18,22 @@
 
           config = {
             allowUnfree = true;
+            cudaSupport = true;
           };
         };
 
         inherit (pkgs) lib;
+        inherit (pkgs) python3;
+
+        python3-pkgs = python3.withPackages (
+          python-pkgs: with python-pkgs; [
+            torch
+          ]
+        );
 
       in
       {
-        devShells.default = pkgs.mkShell (
+        devShells.default = pkgs.mkShellNoCC (
           let
             pre-commit-bin = "${lib.getBin pkgs.pre-commit}/bin/pre-commit";
 
@@ -35,6 +43,7 @@
               black
               mdformat
               pre-commit
+              python3-pkgs
               ruff
               shfmt
               statix
