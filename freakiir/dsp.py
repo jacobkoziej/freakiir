89c0793a7205affdb51a4a93523069db6087b6c3
Jacob Koziej <jacobkoziej@gmail.com>
2025-01-20 00:01:54 -0500

    Define `freqz_zpk()`

--- freakiir/dsp.py
+++ freakiir/dsp.py
@@ -3,9 +3,49 @@
 # dsp.py -- digital signal processing
 # Copyright (C) 2025  Jacob Koziej <jacobkoziej@gmail.com>
 
+import torch
+
 from einops import rearrange
-from torch import Tensor
+from torch import (
+    Tensor,
+    pi,
+)
 
 
 def flatten_sections(h: Tensor) -> Tensor:
     return rearrange(h, "... sections h -> ... (sections h)")
+
+
+def freqz_zpk(
+    z: Tensor,
+    p: Tensor,
+    k: Tensor,
+    *,
+    N: int = 512,
+    whole: bool = False,
+) -> tuple[Tensor, Tensor]:
+    dtype = z.dtype
+    assert p.dtype == dtype
+
+    device = z.device
+    assert p.device == device
+    assert k.device == device
+
+    def polyvalfromroots(x: Tensor, r: Tensor) -> Tensor:
+        r = r.reshape(r.shape + (1,) * x.ndim)
+
+        return torch.prod(x - r, -2)
+
+    k = k.reshape(k.shape + (1,) * (z.ndim - 1))
+
+    end = (2 if whole else 1) * pi
+
+    # our interval is right-open
+    end -= end / N
+
+    w = torch.linspace(0, end, N).to(device)
+    h = torch.exp(1j * w)
+
+    h = k * polyvalfromroots(h, z) / polyvalfromroots(h, p)
+
+    return w, h
--- tests/freakiir/dsp_test.py
+++ tests/freakiir/dsp_test.py
@@ -3,12 +3,23 @@
 # dsp_test.py -- digital signal processing tests
 # Copyright (C) 2025  Jacob Koziej <jacobkoziej@gmail.com>
 
-import torch
+import numpy as np
 import pytest
+import torch
 
+from typing import Final
+
+from scipy import signal
 from torch import Tensor
 
-from freakiir.dsp import flatten_sections
+from freakiir.dsp import (
+    flatten_sections,
+    freqz_zpk,
+)
+
+
+ATOL: Final[float] = 1e-6
+RTOL: Final[float] = 1e-4
 
 
 @pytest.fixture
@@ -62,3 +73,38 @@ def test_flatten_sections(z: Tensor) -> None:
 
     assert z_flat.shape == z_flat_baseline.shape
     assert (z_flat == z_flat_baseline).all()
+
+
+@pytest.mark.parametrize(
+    "N, whole",
+    [
+        (512, False),
+        (512, True),
+        (1024, False),
+        (1024, True),
+    ],
+)
+def test_freqz_zpk(
+    z: Tensor,
+    p: Tensor,
+    k: Tensor,
+    N: int,
+    whole: bool,
+) -> None:
+    z = flatten_sections(z)
+    p = flatten_sections(p)
+
+    w, h = freqz_zpk(z, p, k, N=N, whole=whole)
+
+    z = z.numpy()
+    p = p.numpy()
+    k = k.numpy()
+
+    w = w.numpy()
+    h = h.numpy()
+
+    for z, p, k, h in zip(z, p, k, h):
+        w_scipy, h_scipy = signal.freqz_zpk(z, p, k, worN=N, whole=whole)
+
+        assert np.allclose(w_scipy, w, rtol=RTOL, atol=ATOL)
+        assert np.allclose(h_scipy, h, rtol=RTOL, atol=ATOL)
