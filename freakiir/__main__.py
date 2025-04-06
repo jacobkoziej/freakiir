12d8bc8bd6d16549863a1ef0497b596d1e0c53ab
Jacob Koziej <jacobkoziej@gmail.com>
2025-04-06 02:52:12 -0400

    Define `main()`

---
+++ freakiir/__main__.py
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: MPL-2.0
+#
+# __main__.py -- main
+# Copyright (C) 2025  Jacob Koziej <jacobkoziej@gmail.com>
+
+import sys
+
+
+def main() -> None: ...
+
+
+if __name__ == "__main__":
+    sys.exit(main())
