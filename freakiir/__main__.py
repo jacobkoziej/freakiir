22a161891a2c1ebd4ad571ab960d5a438719f883
Jacob Koziej <jacobkoziej@gmail.com>
2025-04-06 03:29:14 -0400

    Add `train` sub-command

--- freakiir/__main__.py
+++ freakiir/__main__.py
@@ -5,8 +5,36 @@
 
 import sys
 
+import hydra
 
-def main() -> None: ...
+from argparse import ArgumentParser
+
+from omegaconf import DictConfig
+
+
+@hydra.main(version_base="1.2")
+def _train(cfg: DictConfig) -> None: ...
+
+
+def main() -> None:
+    parser = ArgumentParser(
+        description="Frequency Response Enhanced by All-pass estimations of Kth order Infinite Impulse Responses",
+    )
+    subparsers = parser.add_subparsers(
+        dest="sub_command",
+    )
+    _ = subparsers.add_parser("train")
+
+    args, unknownargs = parser.parse_known_args()
+
+    sub_command = args.sub_command or "train"
+
+    match sub_command:
+        case "train":
+            # hydra doesn't let us override its argument vector
+            sys.argv = sys.argv[:1] + unknownargs
+
+            _train()
 
 
 if __name__ == "__main__":
